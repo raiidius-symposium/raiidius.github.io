0f8630ebf4acd8d245dcc2ba76de4ee7878c6109
Harry Reyes Nieva <harryreyesnieva@gmail.com>
2026-03-19 11:33:36 -0400

    init

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { HeroSection } from '@/components/home/hero-section';
 import { AboutSection } from '@/components/home/about-section';
 import { WhatToExpectSection } from '@/components/home/what-to-expect-section';
 import { ThemesSection } from '@/components/home/themes-section';
-import { ImportantDatesSection } from '@/components/home/important-dates-section';
-import { ProgramPreviewSection } from '@/components/home/program-preview-section';
+//import { ImportantDatesSection } from '@/components/home/important-dates-section';
+//import { ProgramPreviewSection } from '@/components/home/program-preview-section';
 import { PartnersSection } from '@/components/home/partners-section';
-import { SpeakersPreviewSection } from '@/components/home/speakers-preview-section';
+//import { SpeakersPreviewSection } from '@/components/home/speakers-preview-section';
 import { MailingListSection } from '@/components/home/mailing-list-section';
 
 export default function HomePage() {
